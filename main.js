cf2606405456649b17eb373c7cd1442bf4fbb274
phạm ngọc <ngoc331998@gmail.com>
2022-12-28 16:51:31 +0700

    add comments

--- main.js
+++ main.js
@@ -2,21 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require ("body-parser");
 const https = require ("https");
-const mongoose = require ("mongoose");                             //kết nối database                    
+const mongoose = require ("mongoose");                             //using mongoose                   
 const session = require('express-session');
 const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');  //plugin để build username và password với passport
-var routes = require(__dirname + '/router.js');               //kết nối với module router
-const date = require(__dirname + '/date.js');                      //kết nối với module date
+const passportLocalMongoose = require('passport-local-mongoose');  //build username and password with passport
+var routes = require(__dirname + '/router.js');                    //connect with module router
+const date = require(__dirname + '/date.js');                      //connect with module date
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-////////Kết nối và sử dụng session để login/////////////////////
+////////Use session to login/////////////////////
 app.use(session({
-    secret: process.env.SECRET,                             //bảo mật bằng salt và hash dãy secret này
+    secret: process.env.SECRET,                             //use salt and hash this field to security
     resave: false,
     saveUninitialized: true,
 
@@ -25,99 +25,99 @@ app.use(passport.initialize());
 app.use(passport.session()); 
 
 
-mongoose.connect('mongodb://localhost:27017/todoListDB');  //Kết nối database
+mongoose.connect('mongodb://localhost:27017/todoListDB');  //connect with database
 mongoose.set('strictQuery',false);
 
-const itemShema = new mongoose.Schema({                     //Tạo lớp itemShema có thuộc tính name với giá trị kiểu String
+const itemShema = new mongoose.Schema({                     //Create itemShema class has property: name, value: String
     name: String
 })
-const Item = mongoose.model('Item', itemShema);             //Khởi tạo constructor 
+const Item = mongoose.model('Item', itemShema);             //Create constructor 
 
 
-const userShema = new mongoose.Schema({                     //Tạo lớp userShema với các thuộc tính:
-    username: String,                                           //username - giá trị kiểu String
-    password: String,                                           //password - giá trị kiểu String
-    Items: [itemShema],                                         //Items    - giá trị kiểu itemShema
+const userShema = new mongoose.Schema({                     //Create userShema with properties:
+    username: String,                                           //username - value: String
+    password: String,                                           //password - value: String
+    Items: [itemShema],                                         //Items    - value: itemShema
 });
 
 userShema.plugin(passportLocalMongoose);                    
 
 const User = mongoose.model('User', userShema);
 
-//Cấu hình Passport
+//Passport configuration
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//module router chứa các phương thức get() đến trang login, register, logout
+//module router contains GET method to login, register, logout page
 app.use('/', routes);
 
-//Biến currentUser lưu giá trị tên username hiện tại
+//variable currentUser save value of current username
 let currentUser ="";
 
-//////////////////Lấy giá trị input khi đăng nhập và đăng ký thông qua method POST/////////////////////
-app.post('/register', function(req,res){                        //method POST lấy request từ trang đăng ký
+//////////////////get input value when signing-up/signing-in through POST method/////////////////////
+app.post('/register', function(req,res){                        
     User.register({username: req.body.username}, req.body.password, function(err, user) {
         if (err) {
             console.log(err);
-            res.render('register');                             //Nếu có lỗi, quay lại trang đăng ký
+            res.render('register');                             //if error, return register page
         }
         else {
             passport.authenticate('local')(req,res,function(){
-                res.redirect('/login');                         //Nếu đăng ký thành công, đến trang login
+                res.redirect('/login');                         //if success, go to login page
             })
         }
     })
 });
-app.post('/login', function(req,res){                        //method POST lấy request từ trang đăng nhập
+app.post('/login', function(req,res){                        
     currentUser = req.body.username;
-    const user = new User({                                  //Tạo 1 instance của lớp User
+    const user = new User({                                  //create an instance of User class
         username: req.body.username,
         password: req.body.password,
 
     });
-    req.login(user, function(err){                          //Phương thức login xác thực user với thông tin đã register trước đó    
+    req.login(user, function(err){                          //login method authenticate user with sign-up information   
         if (err) {
             console.log(err);
         }
         else {
-            //Đăng nhập thành công, đến trang todolist. Nếu không quay lại trang login
+            //if authenticated, go to home page. Or else return login page
             passport.authenticate('local',{ failureRedirect: '/login' })(req,res,function(){
                 res.redirect('/');
-            })          
+            })           
         }
     })
 
 });
 
 
-let day = date.date();                                         //Gọi module date.js hiển thị ngày hôm nay
+let day = date.date();                                         //module date.js show current day
 
-//////////////////////Thêm, sửa, xóa item ghi chú tại home page////////////////////////////
+//////////////////////add, edit, delete items at home page////////////////////////////
 
-app.get("/", function(req,res){                               //Method get() truy cập đến trang chủ todolist
-    if(req.isAuthenticated()) {                               //nếu login đã được xác thực
-        //Tìm trong database theo trường username, trả về kết quả là instance  có username là currentUser
+app.get("/", function(req,res){                               //GET method to home page
+    if(req.isAuthenticated()) {                               //if authenticated
+        //Find username field, return an instance has username is value of currentUser
         User.findOne({username: currentUser}, function(err, showItem){ 
             if (err) console.log(err);
             else{
-                res.render('home', {currentday: day, items: showItem.Items});   //Hiển thị thông tin trường Items
+                res.render('home', {currentday: day, items: showItem.Items});   //show all Items
             }
         })
     }
-    else res.redirect('/login');                                 //Nếu xác thực không thành công, quay lại trang login
+    else res.redirect('/login');                                 //if not authenticated, return login page
 })
 
-//Thêm item ghi chú
+//add items
 app.post("/", function(req,res){                        
-    const note = req.body.newitem;                                      //Lấy giá trị input tại ô nhập giá trị 
-    const item = new Item({                                            //Tạo ghi chú mới là 1 thể hiện của lớp Item
+    const note = req.body.newitem;                                      //get value in input tag having name: newitem
+    const item = new Item({                                             //create new item
         name: note
     });
-    User.findOne({username: currentUser}, function(err, foundItem){     //Truy cập đến username hiện tại
+    User.findOne({username: currentUser}, function(err, foundItem){     //accces to current user
         if (err) console.log(err);
         else {
-            foundItem.Items.push(item);                                //Thêm ghi chú đó vào array Items                       
+            foundItem.Items.push(item);                                //push item into Items arrat                      
             foundItem.save(function(err){
             if(!err) res.redirect('/');
         });
@@ -125,28 +125,28 @@ app.post("/", function(req,res){
     });
 });
 
-//Sửa item ghi chú
+//edit items
 app.post('/update', function(req,res) {                    
-    const update_id = req.body.updateItem;                 //Lấy giá trị id của item cần sửa khi tích vào ô sửa   
-    const updateContent = req.body.updatecontent;          //Lấy giá trị nội dung cần update
-    //Truy cập đến username hiện tại và sửa nội dung của item đó thành updateContent
+    const update_id = req.body.updateItem;                 //get value item having name: updateItem
+    const updateContent = req.body.updatecontent;          //get content after updating
+    //access to current user and update its content
     User.findOneAndUpdate({ username: currentUser, "Items._id": update_id }, {'$set': {'Items.$.name' :updateContent}}, function(err){
         if(err) console.log(err);
         else {
-            res.redirect('/');                             //Quay lại trang todolist
+            res.redirect('/');                             //return to home page
         }
     });
                 
 });
 
-//Xóa item ghi chú
+//delete items
 app.post('/delete', function(req,res) {
-    const checkbox_id = req.body.checkbox;                  //Lấy giá trị id của item cần xóa khi tích vào checkbox      
-    //Truy cập đến username hiện tại và lấy phần tử có id: checkbox_id ra khỏi array Items
+    const checkbox_id = req.body.checkbox;                  //get value item having name: checkbox     
+    //access to current user and pull the item has id: checkbox_id out of Items array
     User.findOneAndUpdate({username: currentUser}, {$pull: {Items: {_id:checkbox_id}}}, function(err){
         if(err) console.log(err);
         else {
-        res.redirect('/');                                  //Quay lại trang todolist
+        res.redirect('/');                                  //return to home page
         }
         });
             
